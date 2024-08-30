deab42e062d225165805cdf174209a4907956485
Franklin <writefranklyn@gmail.com>
2024-08-30 09:44:01 +0100

    add sign up pojo

---
+++ src/main/java/com/example/ecommercejava/model/signup.java
@@ -0,0 +1,43 @@
+package com.example.ecommercejava.model;
+
+import java.io.Serializable;
+
+public class signup implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String email;
+    private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
